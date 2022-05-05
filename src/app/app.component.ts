08dc63e4def127b09d6a8e054472158e33f70eb8
Duilio <duiliobacalorwork>
2022-05-05 14:45:49 -0300

    Primer commit Angular bases

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+ 
+})
+export class AppComponent {
+  titulo:string = 'bases';
+  numero:number = 10;
+  base:number=5;
+
+  acumular(valor:number){
+
+    this.numero += valor;
+ 
+}
+}
