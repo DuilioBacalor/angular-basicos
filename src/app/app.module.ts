a4f62740a42f2a993f76080a79007c0bcad0263e
Duilio <duiliobacalorwork>
2022-05-16 14:26:21 -0300

    commit 160522

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 // import { ContadorModule } from './contador/contador.module';
 //import { ContadorComponent } from './contador/contador/contador.component';
 // import { ContadorComponent } from './contador/contador/contador.component';
@@ -19,8 +20,9 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
-    
+    ContadorModule,
+    DbzModule
+        
   ],
   providers: [],
   bootstrap: [AppComponent]
